aef2356112034c4dbd21a180d9692744ac80e784
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2023-08-02 22:01:54 -0400

    changed order

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -144,6 +144,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         support_one = [[13, 3], [11, 2], [14, 2], [12, 1], [15, 1], [13, 0]]
         wall_two = [[18, 13], [19, 13], [20, 13], [18, 7], [17, 6], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5]]
         turret_three=[[18, 12], [17, 11], [18, 11], [24, 11], [16, 10], [23, 10], [4, 9], [6, 9], [7, 9], [8, 9], [9, 9], [10, 9], [11, 9], [12, 9], [13, 9], [14, 9], [15, 9], [21, 9], [22, 9], [5, 8], [7, 8], [8, 8], [9, 8], [10, 8], [11, 8], [13, 8], [21, 8], [19, 7], [20, 7], [19, 6], [17, 5], [18, 5], [11, 4], [12, 4], [13, 4], [14, 4], [15, 4], [16, 4], [17, 4]]
+        reverse_turret_three=turret_three[::-1]
         support_two = [[18, 12], [17, 11], [24, 11], [16, 10], [23, 10], [15, 9], [22, 9], [21, 8], [20, 7], [7, 6], [19, 6], [18, 5], [17, 4], [14, 3], [15, 2], [16, 2]]
         wall_three = [[2, 13], [3, 12], [17, 12], [4, 11], [16, 11], [5, 10], [6, 10], [7, 10], [8, 10], [9, 10], [10, 10], [11, 10], [12, 10], [13, 10], [14, 10], [15, 10], [10, 5]]
         wall_upgrade_one = [[0, 13], [1, 13], [2, 13], [18, 13], [19, 13], [20, 13], [21, 13], [23, 13], [24, 13], [25, 13], [26, 13], [3, 12], [17, 12], [20, 12], [23, 12], [4, 11], [19, 11], [22, 11], [18, 10], [21, 10], [17, 9], [20, 9], [16, 8], [19, 8]]
@@ -152,9 +153,9 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state.attempt_spawn(TURRET,turret_one)
         game_state.attempt_spawn(TURRET,turret_two)
         game_state.attempt_upgrade(wall_upgrade_one)
-        game_state.attempt_spawn(TURRET,turret_three)
-        game_state.attempt_spawn(SUPPORT,support_one)
         game_state.attempt_spawn(WALL,wall_two)
+        game_state.attempt_spawn(TURRET,reverse_turret_three)
+        game_state.attempt_spawn(SUPPORT,support_one)
         game_state.attempt_upgrade(turret_upgrade_one)
         game_state.attempt_spawn(WALL,wall_three)
         game_state.attempt_spawn(SUPPORT,support_two)
